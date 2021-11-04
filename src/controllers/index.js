fd042cac2b150bc02014b94aa49196b3e5d360f9
Cris Crawford <crawford.cris@gmail.com>
2021-11-04 12:49:32 -0400

    Added database interface

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/export */
 
 export * from './home';
+export * from './messages';
---
+++ src/models/model.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/named */
+import { pool } from './pool';
+
+class Model {
+  constructor(table) {
+    this.pool = pool;
+    this.table = table;
+    this.pool.on('error', (err, client) => `Error, ${err}, on idle client${client}`);
+  }
+
+  async select(columns, clause) {
+    let query = `SELECT ${columns} from ${this.table}`;
+    if (clause) query += clause;
+    return this.pool.query(query);
+  }
+}
+
+export default Model;
